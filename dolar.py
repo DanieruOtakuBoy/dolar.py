971d5f391d3e79ffb0b83401162b03ebccbda48c
DanieruOtakuBoy <130202303+DanieruOtakuBoy@users.noreply.github.com>
2025-03-17 20:00:41 -0400

    Actualización incluyendo futuros precios BCV

--- dolar.py
+++ dolar.py
@@ -1,5 +1,5 @@
 import requests
-
+import datetime
 import locale
 
 from mastodon import Mastodon
@@ -7,12 +7,30 @@ from mastodon import Mastodon
 locale.setlocale(locale.LC_ALL, 'es_VE.UTF-8')
 
 access_token = 'token_de_mastodon'
-mastodon_url = 'url_de_servidor_de_mastodon'
+mastodon_url = 'url_del_servidor_de_mastodon'
 
 mastodon = Mastodon(
 	access_token=access_token,
 	api_base_url=mastodon_url
 )
+
+fecha = datetime.datetime.now()
+dia = fecha.weekday()
+
+if dia == 0:
+    futuro = "El precio del dólar y el euro (BCV) para el día martes será de:"
+
+if dia == 1:
+    futuro = "El precio del dólar y el euro (BCV) para el día miércoles será de:"
+
+if dia == 2:
+    futuro = "El precio del dólar y el euro (BCV) para el día jueves será de:"
+
+if dia == 3:
+    futuro = "El precio del dólar y el euro (BCV) para el día viernes será de:"
+
+if dia == 5 or dia == 6:
+    futuro = "El precio del dólar y el euro (BCV) para la siguiente semana será de:"
 
 response1 = requests.get('https://pydolarve.org/api/v1/dollar?page=criptodolar&monitor=bcv&format_date=default&rounded_price=false')
 response2 = requests.get('https://pydolarve.org/api/v1/dollar?page=criptodolar&monitor=enparalelovzla&format_date=default&rounded_price=false')
@@ -25,7 +43,7 @@ if response1.status_code & response2.status_code & response3.status_code & respo
 	data2 = response2.json()
 	data3 = response3.json()
 	data4 = response4.json()
-	
+
 	for item in data1:
 		dolar_bcv = locale.format_string("%.2f", data1['price_old'], grouping=True)
 		
@@ -56,14 +74,21 @@ if response1.status_code & response2.status_code & response3.status_code & respo
 	for item in data4:
 		tasa_e_par = data4['symbol']
 
+	for item in data1:
+		dolar_fut = locale.format_string("%.2f", data1['price'], grouping=True)
+
+	for item in data1:
+		euro_fut = locale.format_string("%.2f", data3['price'], grouping=True)
+
 		mensaje = (f"¡Hola, Fediverso!\n\nTipo de cambio en #Venezuela para el día de hoy.\n\n"
 				f"Dólar = {dolar_bcv} Bs (BCV) {tasa_d_bcv} | {dolar_par} Bs (paralelo) {tasa_d_par}\n"
 				f"Euro = {euro_bcv} Bs (BCV) {tasa_e_bcv} | {euro_par} Bs (paralelo) {tasa_e_par}\n"
+                f"\n{futuro}\nDólar = {dolar_fut} Bs | Euro = {euro_fut} Bs.\n"
                 f"\nFecha y hora de la última actualización:\nBCV = {time_bcv}\nEnParaleloVzla = {time_par}\n\nFuente: Criptodólar\n"
 				f"\nEsto es todo por ahora, hasta la próxima actualización.")
 
 		mastodon.toot(mensaje)
-		
+
 		print("Toot enviado con éxito.")
 		
 		break
